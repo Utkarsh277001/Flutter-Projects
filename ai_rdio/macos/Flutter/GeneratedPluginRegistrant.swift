cbe970e2dee27eb03716e11ae0a3d896bda4f75c
Utkarsh thakur <thakuradarsh385@gmail.com>
2023-10-14 19:38:19 +0530

    project-2 changes

--- ai_rdio/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ai_rdio/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,19 +5,21 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import geolocator_apple
-import location
-import package_info_plus_macos
+import package_info_plus
 import path_provider_foundation
+import rive_common
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
